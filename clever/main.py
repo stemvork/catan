28591bd2cf257c2c5e391d8a2457182fe4dfac18
Jasper Eenhoorn <eenhoorn.jasper@gmail.com>
2020-12-29 12:35:06 +0100

    Some abstractions. Need to add other coloured Fields.

--- clever/main.py
+++ clever/main.py
@@ -2,6 +2,9 @@ import wasabi2d as w
 import random
 from pprint import pprint
 
+# TODO: abstract play die to fields, show fields if success, die+req if fails
+# TODO: refactor keypress function calls
+
 def dumpobj(args):
     pprint(vars(args))
 
@@ -11,6 +14,8 @@ s.background = 0.1, 0.1, 0.1
 
 b = s.layers[0]
 t = s.layers[1]
+
+state = 'roll'
 
 class Die:
     def __init__(self, color):
@@ -33,6 +38,7 @@ class Die:
         return self.color + " " + str(self.value)
 
 class GreenFields():
+    color  = 'green'
     fields = [None] * 11
     reqs   = list(range(1,6)) + list(range(1, 7))
     boni   = [None, None, None, "again", None,
@@ -52,27 +58,50 @@ class GreenFields():
                               66]
 
     def playnext(die):
-        print('playing', die)
         if die.color == 'green':
             next_index = GreenFields.fields.index(None)
-            print('next_index', next_index)
-            if next_index is None:
-                raise Exception('Green is full.')
-            else:
+            if next_index is not None:
                 if die.value >= GreenFields.reqs[next_index]:
                     GreenFields.fields[next_index] = die
-                    print(GreenFields.fields)
+                    return True
                     # activate boni
                     # update score
-                else:
-                    raise Exception('Value too low:', die.value, 'for',
-                                    GreenFields.reqs[next_index])
-        else:
-            raise Exception('Die is not green.')
 
 dice = Die.newset()
-GreenFields.playnext(dice[3])
-GreenFields.playnext(dice[3])
-GreenFields.playnext(dice[3])
+state = 'select'
 
+def play(die, state, fieldsobj):
+    if fieldsobj.color == die.color:
+        try:
+            nextidx = fieldsobj.fields.index(None)
+        except:
+            nextidx = None
+        if nextidx is not None:
+            if die.value >= fieldsobj.reqs[nextidx]:
+                fieldsobj.fields[nextidx] = die
+                state = 'roll'
+    return die, state, fieldsobj
+
+def roll(dice, state):
+    dice = Die.newset()
+    return dice, 'select'
+
+@w.event
+def on_key_down(key):
+    global state, GreenFields, dice
+
+    if state == 'roll':
+        if key == w.keys.SPACE:
+            dice, state = roll(dice, state)
+
+    if state == 'select':
+        if key in range(w.keys.K_1, w.keys.K_7):
+            die = dice[key-w.keys.K_1]
+            fieldsobj = GreenFields
+            die, state, fieldsobj = play(die, state, fieldsobj)
+
+    print("Dice", dice)
+    print("GreenFields", GreenFields.fields)
+    print("State", state)
+    print()
 w.run()
